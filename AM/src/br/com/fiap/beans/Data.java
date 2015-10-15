855ae27f2d7d81387a782089d6321a45e6dafdd5
Mauricio Junior <junior310595@gmail.com>
2015-10-15 16:24:46 -0300

    Modelo para manipular datas na servlet

---
+++ AM/src/br/com/fiap/beans/Data.java
@@ -0,0 +1,16 @@
+package br.com.fiap.beans;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+public class Data {	
+	public static void main (String[] args) throws ParseException{
+		String data = request.getParameter("data"); // Aqui nós pegamos a String data do formulário
+		SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy"); // Definimos a máscara da data
+		Date dataUsuario=sdf.parse(data); // tranformamos a string recebida em uma Data
+		System.out.println(sdf.format(dataUsuario)); // Printamos a data na mascara definida
+	}
+	
+}
