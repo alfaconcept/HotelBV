b4d0cba36d7105c6bdddedc785c4b4d523d634d7
Leonardo Savoia <leonardo.savoia@outlook.com>
2015-10-20 20:20:01 -0200

    ALteração da Classe ClienteDAO

--- AM/src/br/com/fiap/DAO/ClienteDAO.java
+++ AM/src/br/com/fiap/DAO/ClienteDAO.java
@@ -1,5 +1,21 @@
 package br.com.fiap.DAO;
 
-public class ClienteDAO {
+import java.sql.Connection;
 
+import br.com.fiap.beans.Cliente;
+
+public class ClienteDAO {
+	
+	private Connection conexao;
+	
+	public ClienteDAO() throws Exception {
+		this.conexao = new ConexaoFactory().getConnection();
+	}
+
+	public void insertCliente(Cliente cliente) throws Exception{
+		
+		
+		
+	}
+	
 }
