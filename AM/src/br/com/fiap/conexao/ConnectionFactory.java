1d630d87e1de17b3c186353ce29063f5fdc45c3d
Marcus <Marcus@DELTA-MF>
2015-10-20 20:00:04 -0200

    Alteração na Classe ConexaoFactory

--- AM/src/br/com/fiap/conexao/ConnectionFactory.java
+++ AM/src/br/com/fiap/conexao/ConnectionFactory.java
@@ -1,13 +1,26 @@
 package br.com.fiap.conexao;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
 	
+	private static ConexaoFactory conexao = null;
+	
+	
 	//Conexão com o Banco Oracle
 	public Connection getConnection() throws Exception {
+		FileReader arquivo = new FileReader(System.getProperty("user.dir")+"");
+		BufferedReader dados = new BufferedReader(arquivo);
+		String url = dados.readLine();
+		String usuario = dados.readLine();
+		String senha = dados.readLine();
+		
 		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		//return DriverManager.getConnection(url,usuario,senha);
 		return DriverManager.getConnection("jdbc:oracle:thin:/:@192.168.60.15:1521:ORCL");
 	}
 
