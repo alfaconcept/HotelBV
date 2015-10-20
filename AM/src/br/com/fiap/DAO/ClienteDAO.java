460508ec48a957e8058ccd472b90d2a2da8179df
Leonardo Savoia <leonardo.savoia@outlook.com>
2015-10-20 20:30:21 -0200

    Alteração classe ClienteDAO

--- AM/src/br/com/fiap/DAO/ClienteDAO.java
+++ AM/src/br/com/fiap/DAO/ClienteDAO.java
@@ -3,16 +3,20 @@ package br.com.fiap.DAO;
 import java.sql.Connection;
 
 import br.com.fiap.beans.Cliente;
+import br.com.fiap.conexao.ConnectionFactory;
 
 public class ClienteDAO {
 	
-	private Connection conexao;
+
 	
 	public ClienteDAO() throws Exception {
-		this.conexao = new ConexaoFactory().getConnection();
+		
 	}
 
 	public void insertCliente(Cliente cliente) throws Exception{
+		
+		String sql = "INSERT INTO T_AM_AFC_CLIENTE(CD_CLIENTE,NR_CPF,NR_RG,DT_NASCIMENTO,NR_QUARTO_PREFERIDO,DS_EMAIL,DS_SENHA)" + 
+				"VALUES(?,?,?,?,?,?,?,?)";
 		
 		
 		
