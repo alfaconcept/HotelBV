47b489d790405eb52b57d36fcf524f89dd9d9859
Mauricio Junior <junior310595@gmail.com>
2015-10-22 09:29:34 -0200

    Atualizações na ServletReserva e Web.xml Adição do tratamento de erros na Servlet Adição de redirecionamento de erros no Web.xml

--- AM/src/br/com/fiap/controller/ServletReserva.java
+++ AM/src/br/com/fiap/controller/ServletReserva.java
@@ -3,6 +3,8 @@ package br.com.fiap.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,6 +19,7 @@ import br.com.fiap.BO.ReservaBO;
 import br.com.fiap.beans.Cliente;
 import br.com.fiap.beans.Quarto;
 import br.com.fiap.beans.Reserva;
+import br.com.fiap.conexao.ConnectionFactory;
 
 /**
  * Servlet implementation class ServletReserva
@@ -48,9 +51,11 @@ public class ServletReserva extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		if (request.getParameter("action").equals("reservar")) {
+		if (request.getParameter("action").equals("reservar")){
+				Connection conn = null;
 			try {				
-//				Connection conn = ConnectionFactory.controlarInstancia().getConnection();	
+				conn = ConnectionFactory.controlarInstancia().getConnection();	
+				conn.setAutoCommit(false);
 				
 				// Obtendo os dados do cliente que esta fazendo a reserva
 				Cliente cliente = new Cliente();
@@ -90,9 +95,22 @@ public class ServletReserva extends HttpServlet {
 				session.setAttribute("reserva", reserva);
 				request.getRequestDispatcher("reserva.jsp").forward(request, response);
 			} catch (Exception e) {
+				try {
+					conn.rollback();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				StringWriter errors = new StringWriter();
 				e.printStackTrace(new PrintWriter(errors));
 				response.getWriter().print(errors.toString());
+			} finally {
+				try {
+					conn.close();
+				} catch (Exception e2) {
+					// TODO: handle exception
+					
+				}
 			}
 		}
 		
