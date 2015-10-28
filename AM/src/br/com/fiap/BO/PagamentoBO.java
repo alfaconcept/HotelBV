1244653a4e40d8068373a283ad01fc63a388f43d
Marcus <Marcus@DELTA-MF>
2015-10-28 20:59:39 -0200

    Alteração na Classe PagamentoBO

--- AM/src/br/com/fiap/BO/PagamentoBO.java
+++ AM/src/br/com/fiap/BO/PagamentoBO.java
@@ -2,9 +2,10 @@ package br.com.fiap.BO;
 
 import java.sql.Connection;
 
-
 import br.com.fiap.DAO.PagamentoDAO;
+import br.com.fiap.beans.Hospedagem;
 import br.com.fiap.beans.Pagamento;
+import br.com.fiap.excecoes.Excecoes;
 
 /**
 * 
@@ -37,5 +38,15 @@ public class PagamentoBO {
 		new PagamentoDAO().insertPagamentoCartao(pagamento, conexao);
 		
 		return 1;
+	}
+	
+	public Pagamento retornaPagamento(Hospedagem hospedagem, Connection conn) throws Exception{
+		
+		if(hospedagem.getCdHospedagem() == 0){
+			throw new Excecoes("Código da Hospedagem não informado");
+		}
+		
+		return new PagamentoDAO().returnPagamento(hospedagem, conn);
+				
 	}
 }
